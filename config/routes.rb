4376dc5a7124af9a9732b08320eabe8a8e40c529
Aashaka Shah <aashaka96@gmail.com>
2016-03-26 19:30:49 +0530

    Starts with login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+
   root 'static_pages#home'
   get 'signup' => 'users#new'
   resources :users
