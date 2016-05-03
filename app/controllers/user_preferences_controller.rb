ebd711e6d046d8853c529cb93b492563dd33f1c7
Aashaka Shah <aashaka96@gmail.com>
2016-05-03 09:48:54 +0530

    Add feature to save UserPreference
    This is done in a way that query for preference can be easily made by
    using the stored values of source and category.

--- app/controllers/user_preferences_controller.rb
+++ app/controllers/user_preferences_controller.rb
@@ -8,19 +8,36 @@ class UserPreferencesController < ApplicationController
   end
 
   def create
-    @user_preference = UserPreference.new(user_preference_params)
-    if @user_preference.save
-      flash[:success] = "Preference saved"
-      redirect_to '/news'
-    else
-      render 'new'
-    end
+    id = session[:user_id]
+    user_preference = add_query(id, params[:preferences])
   end
 
   private
 
     def user_preference_params
-      params[:preferences][:UID] = session[:user_id]
+      
       user_prefs = params.require(:preferences).permit(:source, :category, :UID)
     end
+
+    def add_query(id, preferences)
+      if user_preference = UserPreference.find_by(UID: id)
+        preferences[:source] = user_preference[:source] + "%20OR%20" + preferences[:source]
+        preferences[:category] = user_preference[:category] + "%20OR%20" + preferences[:category]
+        if user_preference.update(source: preferences[:source], category: preferences[:category])
+          flash[:success] = "Preference saved"
+          redirect_to '/news'
+        else
+        render 'new'
+        end
+      else
+        preferences[:UID] = id
+        if user_preference.save(preferences)
+          flash[:success] = "Preference saved"
+          redirect_to '/news'
+        else
+          render 'new'
+        end
+      end
+    end
+
 end
